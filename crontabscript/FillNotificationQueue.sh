2aaaac9f6cc0b66fd0230d0d63a62285ea174230
isamllr <isabel.mueller@gmail.com>
2014-07-22 18:39:48 +0200

    updates

--- crontabscript/FillNotificationQueue.sh
+++ crontabscript/FillNotificationQueue.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 #shell script that will run php script
 #FillNotificationQueue.php
-export TZ="/usr/share/zoneinfo/Europe/Kiev"
-now=$(date +"%Y-%m-%d")
-adayago="$(date -d '1 day ago' +"%Y-%m-%d")"
-
-#rm -f Reminders*.csv
-#rm -f RemindersHosp*.csv
-#rm -f Recommendations*.csv
 
 if ps -ef | grep -v grep | grep FillNotificationQueue.php ; then
        echo 'Cronjob FillNotificationQueue already running'
@@ -16,12 +9,25 @@ else
 #       php /opt/lampp/htdocs/mch-registry/crontabscript/FillNotificationQueue.php
 fi
 
-#TODO: do for all 7 days, if not exists....
 cd google_appengine
-echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_reminders.yaml --filename=Reminders$now.csv --kind=Reminders --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com
 
-echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_reminders.yaml --filename=RemindersHosp$now.csv --kind=Reminders --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com
+startDate=$(date +"%Y-%m-%d")
+numDays=7
 
-echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_recommendations.yaml --filename=Recommendations$now.csv --kind=Recommendations --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com
+for i in `seq 1 $numDays`
+do 
+	useDate=`date +%Y-%m-%d -d "${startDate}-${i} days"`
+
+	echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_reminders.yaml --filename=Reminders$useDate.csv --kind=Reminders --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com --passin
+
+	echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_reminders.yaml --filename=RemindersHosp$useDate.csv --kind=Reminders --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com --passin
+
+	echo 'swisstphukraine' | python appcfg.py upload_data --config_file=bulkloader_recommendations.yaml --filename=Recommendations$useDate.csv --kind=Recommendations --url=http://mch-registry.appspot.com/remote_api --email=mchregistry@gmail.com --passin
+
+done
+
+rm -f Reminders*.csv
+rm -f RemindersHosp*.csv
+rm -f Recommendations*.csv
 
 exit 0
