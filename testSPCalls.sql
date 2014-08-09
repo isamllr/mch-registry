0d954cae2c1d38c6fe84e12163ede590260e9b92
isamllr <isabel.mueller@gmail.com>
2014-08-09 14:57:52 +0200

    next release sql file

--- testSPCalls.sql
+++
@@ -1,9 +0,0 @@
-CALL `registry`.`fillRecommendationNotificationForPatients`(current_date, 'UK', '10:00:00', 1, 1, @nrOfNotifications);
-
-CALL `registry`.`fillVisitReminderNotificationForPatients`(current_date, 'UK', 1, '10:00:00', 1, 1, @nrOfNotifications);
-
-CALL `registry`.`fillVisitReminderHospitalisationNotificationForPatients`(current_date, 'UK', 1, '10:00:00', 1, 1, @nrOfNotifications);
-
-
-
-
